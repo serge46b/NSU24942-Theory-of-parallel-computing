509c70e2ca700b0f35be4ade6a9bd3503bf16c20
Serge46 <serg46b@gmail.com>
2026-02-10 16:22:34 +0700

    Rewrited on dynamic arrays

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,6 +1,7 @@
 #include <math.h>
 
 #include <iostream>
+#include <vector>
 
 #ifdef USE_DOUBLE
 #define ARR_TYPE double
@@ -9,17 +10,18 @@
 #endif
 
 #define ELEMENT_AMOUNT 10000000
-ARR_TYPE res_arr[ELEMENT_AMOUNT];
 
 int main() {
   std::cout << "ARR_TYPE: "
             << (std::is_same<ARR_TYPE, double>::value ? "double" : "float")
             << std::endl;
+  std::vector<ARR_TYPE> res_arr;
   std::cout << "Calculation started..." << std::endl;
   ARR_TYPE elem_sum = 0;
   for (int i = 0; i < ELEMENT_AMOUNT; i++) {
-    res_arr[i] = sin(i * 2 * 3.14 / ELEMENT_AMOUNT);
-    elem_sum += res_arr[i];
+    ARR_TYPE elem = (ARR_TYPE)sin(i * 2 * 3.14159265359 / ELEMENT_AMOUNT);
+    res_arr.push_back(elem);
+    elem_sum += elem;
   }
   std::cout << "Sum: " << elem_sum << std::endl;
 }
